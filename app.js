7a06f01de7d8fef854383220b74095862c5ef517
yamaso-burger <apple_1228_ringo@yahoo.co.jp>
2022-09-24 11:35:18 +0900

    insert many items using mongoose

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const fruitSchema = new mongoose.Schema ({
     review: String
 });
 
+
 const Fruit = mongoose.model("Fruit",fruitSchema);
 
 const fruit = new Fruit({
@@ -16,4 +17,44 @@ const fruit = new Fruit({
     review: "Pretty solid as a fruit."
 });
 
-fruit.save();
+// fruit.save();
+
+const personSchema = mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    age: Number
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+const person = new Person({
+    firstName: "Soichiro",
+    lastName: "Yamauchi",
+    age: 24
+});
+
+// person.save();
+
+const kiwi = new Fruit({
+    name : "Kiwi",
+    score: 3,
+    review: "The best fruit!"
+});
+const orange = new Fruit({
+    name : "Orange",
+    score: 2,
+    review: "Too sour for me."
+});
+const banana = new Fruit({
+    name: "Banana",
+    score: 3,
+    review: "Weird texture"
+});
+
+Fruit.insertMany([kiwi, orange, banana], function(err){
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Successfully saved all the fruits to fruitsDB");
+    }
+});
